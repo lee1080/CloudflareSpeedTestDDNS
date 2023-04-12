aba417d8b4defcc1dc55e62223e71ab8a43ec437
lee1080 <38096314+lee1080@users.noreply.github.com>
2023-04-12 20:08:38 +0800

    Delete cf_RE.sh

--- cf_RE.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-##用于openwrt安装jq和timeout，确保CloudflareSpeedTestDDNS运行正常。
-opkg update
-opkg install jq coreutils-timeout
-opkg list-installed | grep jq
-opkg list-installed | grep coreutils-timeout
