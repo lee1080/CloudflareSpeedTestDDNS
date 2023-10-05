17befab5395413a2dd01a060554c9230199836b6
stephenzwj <stephenzwj@users.noreply.github.com>
2023-10-06 02:00:51 +0800

    Set config.conf in the repo as the default and allow user to partly change the default settings

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Default config file
 config_file="config.conf"
-
+# Source the default config file to get all default config
+source "$config_file"
 # Check if --cfg parameter is provided
 for i in "$@"
 do
@@ -10,11 +11,10 @@ do
         # Get the next argument as the config file
         shift
         config_file="$1"
+        # Source the config file given by user and modify the default
+        source "$config_file"
     fi
 done
-
-# Source the config file
-source "$config_file"
 
 source ./cf_ddns/cf_check.sh
 
