9b0850c7daf6adede92344155fa9d11af0a151d2
stephenzwj <stephenzwj@users.noreply.github.com>
2023-10-06 09:49:42 +0800

    add the support of region selection

--- cf_ddns/cf_ddns_cloudflare.sh
+++ cf_ddns/cf_ddns_cloudflare.sh
@@ -102,18 +102,18 @@ CFCOLO_PARAM_STRING=
 if [ -z "$CFCOLO" ]; then
     echo "CFCOLO is empty, searching for IP globally"
 else
-    echo "Searching for IPs in ${CFCOLO}."
     CFCOLO_PARAM_STRING="-cfcolo ${CFCOLO}"
+    echo "Searching for IPs in ${CFCOLO} by using ${CFCOLO_PARAM_STRING}."
 fi
 
 if [ "$IP_PR_IP" -ne "0" ] ; then
-  $CloudflareST $CFST_URL_R -t $CFST_T -n $CFST_N -dn $CFST_DN -tl $CFST_TL -dt $CFST_DT -tp $CFST_TP -sl $CFST_SL -p $CFST_P -tlr $CFST_TLR $CFST_STM -f ./cf_ddns/pr_ip.txt -o ./cf_ddns/result.csv CFCOLO_PARAM_STRING
+  $CloudflareST $CFST_URL_R $CFCOLO_PARAM_STRING -t $CFST_T -n $CFST_N -dn $CFST_DN -tl $CFST_TL -dt $CFST_DT -tp $CFST_TP -sl $CFST_SL -p $CFST_P -tlr $CFST_TLR $CFST_STM -f ./cf_ddns/pr_ip.txt -o ./cf_ddns/result.csv
 elif [ "$IP_ADDR" = "ipv6" ] ; then
   #开始优选IPv6
-  $CloudflareST $CFST_URL_R -t $CFST_T -n $CFST_N -dn $CFST_DN -tl $CFST_TL -dt $CFST_DT -tp $CFST_TP -tll $CFST_TLL -sl $CFST_SL -p $CFST_P -tlr $CFST_TLR $CFST_STM -f ./cf_ddns/ipv6.txt -o ./cf_ddns/result.csv CFCOLO_PARAM_STRING
+  $CloudflareST $CFST_URL_R $CFCOLO_PARAM_STRING -t $CFST_T -n $CFST_N -dn $CFST_DN -tl $CFST_TL -dt $CFST_DT -tp $CFST_TP -tll $CFST_TLL -sl $CFST_SL -p $CFST_P -tlr $CFST_TLR $CFST_STM -f ./cf_ddns/ipv6.txt -o ./cf_ddns/result.csv
 else
   #开始优选IPv4
-  $CloudflareST $CFST_URL_R -t $CFST_T -n $CFST_N -dn $CFST_DN -tl $CFST_TL -dt $CFST_DT -tp $CFST_TP -tll $CFST_TLL -sl $CFST_SL -p $CFST_P -tlr $CFST_TLR $CFST_STM -f ./cf_ddns/ip.txt -o ./cf_ddns/result.csv CFCOLO_PARAM_STRING
+  $CloudflareST $CFST_URL_R $CFCOLO_PARAM_STRING -t $CFST_T -n $CFST_N -dn $CFST_DN -tl $CFST_TL -dt $CFST_DT -tp $CFST_TP -tll $CFST_TLL -sl $CFST_SL -p $CFST_P -tlr $CFST_TLR $CFST_STM -f ./cf_ddns/ip.txt -o ./cf_ddns/result.csv
 fi
 echo "测速完毕";
 
