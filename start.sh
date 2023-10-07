5e2c3ca10d0e377f9dc261780e8fd2db5fb94afd
stephenzwj <stephenzwj@users.noreply.github.com>
2023-10-07 20:44:23 +0800

    set config.conf as default to allow users to only define the configurations that are necessary

--- start.sh
+++ start.sh
@@ -2,19 +2,19 @@
 # Default config file
 config_file="config.conf"
 
+# Source the default config file to get all default config
+source "$config_file"
 # Check if --cfg parameter is provided
 for i in "$@"
 do
-    if [ "$i" == "--cfg" ]
-    then
+@@ -10,12 +11,11 @@ do
         # Get the next argument as the config file
         shift
         config_file="$1"
+        # Source the config file given by user and modify the default
+        source "$config_file"
     fi
 done
-
-# Source the config file
-source "$config_file"
 
 source ./cf_ddns/cf_check.sh
 
